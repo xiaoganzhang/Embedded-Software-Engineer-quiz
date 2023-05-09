c558b4bc1327706606ada49c1b3ce8ac3ea86458
xiaoganzhang <77029833+xiaoganzhang@users.noreply.github.com>
2023-05-09 00:17:18 -0600

    Update main.c

--- main.c
+++ main.c
@@ -16,11 +16,6 @@ Output: 9x9
 
 #include <stdio.h>
 
-int gcd(int a, int b) {
-    if (b == 0)
-        return a;
-    return gcd(b, a % b);
-}
 
 void find_squares(int n, int m) {
     while (n != 0 && m != 0) {
